4e70839db0fff67cd99d0758ba68252bb6583310
ElTigre001kz <121248984+ElTigre001kz@users.noreply.github.com>
2023-10-05 01:19:39 +0600

    Update loadPlayer.js

--- js/loadPlayer.js
+++ js/loadPlayer.js
@@ -29,24 +29,6 @@
 		window.addEventListener("message", function (event) {
 			if (event.data.event === 'init') {
 				// document.getElementById('videoiframe').contentWindow.postMessage({"api": "fullscreen"}, "*");
-let iframe = document.getElementById('play');
-
-// Функция для запроса полноэкранного режима
-function requestFullscreen() {
-  if (iframe.requestFullscreen) {
-    iframe.requestFullscreen();
-  } else if (iframe.mozRequestFullScreen) { // Firefox
-    iframe.mozRequestFullScreen();
-  } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari и Opera
-    iframe.webkitRequestFullscreen();
-  } else if (iframe.msRequestFullscreen) { // Internet Explorer
-    iframe.msRequestFullscreen();
-  }
-}
-
-// Вызываем функцию requestFullscreen() после загрузки всего документа
-window.addEventListener('DOMContentLoaded', requestFullscreen);
-
 
 				
 			}
