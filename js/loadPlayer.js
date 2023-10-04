eb68ad3cc37e1606456b3dcc2f1b8051c973caf1
ElTigre001kz <121248984+ElTigre001kz@users.noreply.github.com>
2023-10-05 00:44:48 +0600

    Update loadPlayer.js

--- js/loadPlayer.js
+++ js/loadPlayer.js
@@ -10,28 +10,8 @@
 			  video.width = '100%';
 			  video.height = '100%';
 			  // video.setAttribute('allowFullScreen', '');
-			     // video.setAttribute('allow', 'fullscreen');
-			     // Получаем ссылку на iframe и элемент с id "play"
-let iframe = document.querySelector('iframe');
-let playButton = document.getElementById('play');
-
-// Функция для запроса полноэкранного режима
-function requestFullscreen() {
-  // Проверяем поддержку Fullscreen API в текущем браузере
-  if (iframe.requestFullscreen) {
-    iframe.requestFullscreen();
-  } else if (iframe.mozRequestFullScreen) { // Firefox
-    iframe.mozRequestFullScreen();
-  } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari и Opera
-    iframe.webkitRequestFullscreen();
-  } else if (iframe.msRequestFullscreen) { // Internet Explorer
-    iframe.msRequestFullscreen();
-  }
-}
-
-// Добавляем обработчики событий для iframe и кнопки "play"
-iframe.addEventListener('click', requestFullscreen);
-playButton.addEventListener('click', requestFullscreen);
+			     video.setAttribute('allow', 'fullscreen');
+			     
 			     
 			  document.querySelector('#play').appendChild(video);
 			
